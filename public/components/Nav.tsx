48cb6e57c903f4fb64ad1f5a9c65e83b2fc6b66e
Puru Vijay <devpuruvj@gmail.com>
2021-05-06 01:12:09 +0530

    use intersection observer for nav shadow

--- public/components/Nav.tsx
+++ public/components/Nav.tsx
@@ -6,53 +6,47 @@ import { throttle } from 'throttle-debounce';
 import css from './Nav.module.scss';
 import { SiteLogoSVG } from './svg/SiteLogoSVG';
 import { ThemeSwitcher } from './ThemeSwitcher';
+import { useInView } from 'react-intersection-observer';
 
 export const Nav = () => {
-  const [scrollY, setScrollY] = useState(0);
   const { path } = useLocation();
+  const [ref, inView] = useInView({ initialInView: true, threshold: 0 });
 
   useTimeout(() => {
     document.body.style.setProperty('--transition-duration', '200ms');
   }, 200);
 
-  const handleScroll = () => {
-    setScrollY(document.documentElement.scrollTop);
-  };
-
-  useEffect(() => {
-    const throttledHandleScroll = throttle(50, false, handleScroll);
-
-    document.body.onscroll = throttledHandleScroll;
-  }, []);
-
   return (
-    <nav class={clsx(css.nav, scrollY > 2 && css.shadow)}>
-      <ul class={css.navLinksList}>
-        <li>
-          <a aria-current={path === '/' && 'page'} href=".">
-            HOME
-          </a>
-        </li>
-        <li>
-          <a aria-current={path.startsWith('/blog') && 'page'} href="/blog">
-            BLOG
-          </a>
-        </li>
-        <li>
-          <a aria-current={path.startsWith('/works') && 'page'} href="/works">
-            WORKS
-          </a>
-        </li>
-      </ul>
+    <>
+      <div ref={ref} style={{ position: 'absolute', top: 0, width: '100%', height: '2px' }}></div>
+      <nav class={clsx(css.nav, !inView && css.shadow)}>
+        <ul class={css.navLinksList}>
+          <li>
+            <a aria-current={path === '/' && 'page'} href=".">
+              HOME
+            </a>
+          </li>
+          <li>
+            <a aria-current={path.startsWith('/blog') && 'page'} href="/blog">
+              BLOG
+            </a>
+          </li>
+          <li>
+            <a aria-current={path.startsWith('/works') && 'page'} href="/works">
+              WORKS
+            </a>
+          </li>
+        </ul>
 
-      <div class={css.brand}>
-        <SiteLogoSVG />
-        <span>puruvj.dev</span>
-      </div>
-      <span />
-      <span class={css.themeSwitcher}>
-        <ThemeSwitcher />
-      </span>
-    </nav>
+        <div class={css.brand}>
+          <SiteLogoSVG />
+          <span>puruvj.dev</span>
+        </div>
+        <span />
+        <span class={css.themeSwitcher}>
+          <ThemeSwitcher />
+        </span>
+      </nav>
+    </>
   );
 };
